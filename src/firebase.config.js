818c8ed3edfb20fd01d279f5f54a08824ee8195f
koushik reddy <koushikreddy2005@outlook.com>
2023-06-23 11:37:19 -0700

    added a file to .gitignore

---
+++ src/firebase.config.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore'
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA5GGleqhCHAk50-4Ejd1gNNaOeoNT16Lk",
+  authDomain: "house-marketplace-app-31860.firebaseapp.com",
+  projectId: "house-marketplace-app-31860",
+  storageBucket: "house-marketplace-app-31860.appspot.com",
+  messagingSenderId: "410747818833",
+  appId: "1:410747818833:web:1d193f4af2c67b40fff970"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore()
+export const auth = getAuth(app)
+export default app;
